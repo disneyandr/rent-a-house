d2652348b0c5e953934dcf2b26dedc4ca9cd77dc
Disney <disneyandrade@gmail.com>
2023-12-27 16:33:45 -0300

    v2.4

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from './app';
 
-const port = process.env.PORT;
+// Obtém a porta da variável de ambiente, ou usa a porta 8080 como padrão
+const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
-    console.log(`listening on port:${port}`);
+    console.log(`Listening on port: ${port}`);
 });
